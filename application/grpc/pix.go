7507c05310c9de553dfc3563039dfea72d7fb68f
Rodrigo Rael <rodrigorael53@gmail.com>
2021-05-14 12:21:14 -0300

    implement the pixKey RegisterKey service in GRPC

---
+++ application/grpc/pix.go
@@ -0,0 +1,28 @@
+package grpc
+
+import (
+	"context"
+
+	"github.com/Rodrigo001-de/code-pix/application/grpc/pb"
+	"github.com/Rodrigo001-de/code-pix/application/usecase"
+)
+
+type PixGrpcService struct {
+	PixUseCase usecase.PixUseCase
+	pb.UnimplementedPixServiceServer
+}
+
+func (p *PixGrpcService) RegisterPixKey(ctx context.Context, in *pb.PixKeyRegistration) (*pb.PixKeyCreatedResult, error) {
+	key, err := p.PixUseCase.RegisterKey(in.Key, in.Kind, in.AccountId)
+	if err != nil {
+		return &pb.PixKeyCreatedResult{
+			Status: "not created",
+			Error:  err.Error(),
+		}, err
+	}
+
+	return &pb.PixKeyCreatedResult{
+		Id:     key.ID,
+		Status: "created",
+	}, nil
+}
